f3eca3f66976a788d388480488ff2556065014e8
unknown <thducuit@gmail.com>
2019-06-14 07:05:34 +0700

    write an component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,32 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-import { HomeComponent } from './containers/home/home.component';
-import { AboutComponent } from './containers/abouts/about/about.component';
+import {HomeComponent} from './containers/home/home.component';
+import {AboutComponent} from './containers/abouts/about/about.component';
 
-import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
-import { PageLayoutComponent } from './layouts/page-layout/page-layout.component';
+import {HomeLayoutComponent} from './layouts/home-layout/home-layout.component';
+import {PageLayoutComponent} from './layouts/page-layout/page-layout.component';
 
 const routes: Routes = [
-	{ 
-        path: '', 
-        component: HomeLayoutComponent,
-        children: [
-          { path: '', component: HomeComponent, pathMatch: 'full'}
-        ]
-    },
-
-    { 
-        path: '',
-        component: PageLayoutComponent, 
-        children: [
-          { path: 'about', component: AboutComponent }
-        ]
-    },
+  {
+    path: '',
+    component: HomeLayoutComponent,
+    children: [
+      {path: '', component: HomeComponent, pathMatch: 'full'}
+    ]
+  },
+  {
+    path: '',
+    component: PageLayoutComponent,
+    children: [
+      {path: 'about', component: AboutComponent}
+    ]
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/components/booking-specialty/booking-specialty.component.ts
+++ src/app/components/booking-specialty/booking-specialty.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-booking-specialty',
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingSpecialtyComponent implements OnInit {
 
-  constructor() { }
+  public isActive: boolean;
+
+  private wasInside = false;
+
+  @HostListener('click')
+  clickInside() {
+    this.wasInside = true;
+  }
+
+  @HostListener('document:click')
+  clickout() {
+    if (!this.wasInside) {
+      this.isActive = false;
+    }
+    this.wasInside = false;
+  }
+
+  constructor() {
+    this.isActive = false;
+  }
 
   ngOnInit() {
+
+  }
+
+  handleInputClick() {
+    this.isActive = true;
   }
 
 }
