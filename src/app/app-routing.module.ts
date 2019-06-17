56635594291f2fd4ae3181564e6df644d3e549af
Dung.Dang <dungjk@gmail.com>
2019-06-17 23:23:26 +0700

    Update main router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,13 @@ import {AboutComponent} from './containers/abouts/about/about.component';
 
 import {HomeLayoutComponent} from './layouts/home-layout/home-layout.component';
 import {PageLayoutComponent} from './layouts/page-layout/page-layout.component';
+import { QaComponent } from './containers/abouts/qa/qa.component';
+import { TestimonialComponent } from './containers/abouts/testimonial/testimonial.component';
+import { ServiceComponent } from './containers/services/service/service.component';
+import { InsuranceComponent } from './containers/services/insurance/insurance.component';
+import { DoctorComponent } from './containers/doctor/doctor.component';
+import { VideoComponent } from './containers/news/video/video.component';
+import { EventComponent } from './containers/news/event/event.component';
 
 const routes: Routes = [
   {
@@ -19,14 +26,27 @@ const routes: Routes = [
     path: '',
     component: PageLayoutComponent,
     children: [
-      {path: 'about', component: AboutComponent}
+      { path: 'about-us/aih-hospital', component: AboutComponent },
+      { path: 'about-us/faq', component: QaComponent },
+      { path: 'about-us/testimonial', component: TestimonialComponent },
+      { path: 'patient-services/medical-services', component: ServiceComponent },
+      { path: 'patient-services/medical-package', component: ServiceComponent },
+      { path: 'patient-services/insurance', component: InsuranceComponent },
+      { path: 'patient-services/factsheet', component: ServiceComponent },
+      { path: 'doctor', component: DoctorComponent },
+      { path: 'news', component: EventComponent },
+      { path: 'videos', component: VideoComponent }
     ]
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: true,
+    useHash: false
+  })],
+  exports: [RouterModule],
+  providers: []
 })
 export class AppRoutingModule {
 }
