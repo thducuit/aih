27db6d87e3ad66367d3ab69f4e0b0a9c9d133a1b
Dung.Dang <dungjk@gmail.com>
2019-07-15 00:10:38 +0700

    Update page title and favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,27 +2,41 @@ import { Component, AfterViewInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { getLanguage } from './utilities';
 import { GlobalEventService } from './services/global-event.service';
-import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+import {
+  Router,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError,
+} from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements AfterViewInit {
-  title = 'aih-app';
   loading = false;
 
   constructor(
     private translate: TranslateService,
     private globalEventService: GlobalEventService,
-    private router: Router
+    private router: Router,
+    private title: Title,
   ) {
     // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('en');
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
     this.translate.use(getLanguage()); // Activappe current language or default language
+
+    // Set app title
+    this.updateTitle();
+    translate.onLangChange.subscribe(() => {
+      this.updateTitle();
+    });
 
     // Listen for global events
     globalEventService
@@ -53,5 +67,14 @@ export class AppComponent implements AfterViewInit {
 
   switchLanguage(language: string) {
     this.translate.use(language);
+  }
+
+  updateTitle() {
+    forkJoin(
+      this.translate.get('home'),
+      this.translate.get('american_international_hospital'),
+    ).subscribe(([home, aihHospital]) => {
+      this.title.setTitle(`${home} - ${aihHospital}`);
+    });
   }
 }
