3fd5e2f01508d3f0fa252d1b8c75195e94c598d6
Dung.Dang <dungjk@gmail.com>
2019-07-15 00:54:20 +0700

    Fix layout

--- src/app/layouts/home-layout/home-layout.component.ts
+++ src/app/layouts/home-layout/home-layout.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { getLanguage } from 'src/app/utilities';
 
 @Component({
   selector: 'app-home-layout',
   templateUrl: './home-layout.component.html',
-  styleUrls: ['./home-layout.component.scss']
+  styleUrls: ['./home-layout.component.scss'],
 })
 export class HomeLayoutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  get pageClasses() {
+    const language = getLanguage();
+    return [language, 'window'];
   }
-
 }
