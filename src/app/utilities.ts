620eaedcd5a26bfdcb511abe388070b007d7e97a
Dung.Dang <dungjk@gmail.com>
2019-07-18 01:45:24 +0700

    Style news component on home page

--- src/app/utilities.ts
+++ src/app/utilities.ts
@@ -9,3 +9,34 @@
 // export function setLanguage(lang: string) {
 //   localStorage && localStorage.setItem('lang', lang);
 // }
+
+export function CalculatePagination(current: number, total: number) {
+  const last = total;
+  const delta = 2;
+  const left = current - delta;
+  const right = current + delta + 1;
+  const range = [];
+  const rangeWithDots = [];
+  let l: number;
+
+  for (let i = 1; i <= last; i++) {
+    if (i === 1 || i === last || (i >= left && i < right)) {
+      range.push(i);
+    }
+  }
+
+  for (let i = 0; i < range.length; i++) {
+    const num = range[i];
+    if (l) {
+      if (num - l === 2) {
+        rangeWithDots.push(l + 1);
+      } else if (num - l !== 1) {
+        rangeWithDots.push('...');
+      }
+    }
+    rangeWithDots.push(num);
+    l = num;
+  }
+
+  return rangeWithDots;
+}
