a9be665001e7f7f08da6bd35f0ef73bb6d698fdf
dungjk <dungjk@gmail.com>
2019-07-18 09:33:42 +0700

    Fix linting

--- src/app/containers/news/news-detail/news-detail.component.ts
+++ src/app/containers/news/news-detail/news-detail.component.ts
@@ -7,28 +7,25 @@ import { UrlService } from '../../../services/url.service';
 @Component({
   selector: 'app-news-detail',
   templateUrl: './news-detail.component.html',
-  styleUrls: ['./news-detail.component.scss']
+  styleUrls: ['./news-detail.component.scss'],
 })
 export class NewsDetailComponent implements OnInit {
-
   public blog: Blog;
-  constructor(private route: ActivatedRoute,
-              public postService: PostService) { }
+  constructor(private route: ActivatedRoute, public postService: PostService) {}
 
   ngOnInit() {
     // const alias = this.route.snapshot.paramMap.get('alias');
     this.route.paramMap.subscribe(params => {
-      const alias = params.get("alias");
-      this.postService.fetch(alias).subscribe((data: {}) => {
-        const blog = new Blog(data['Post']);
+      const alias = params.get('alias');
+      this.postService.fetch(alias).subscribe((data: any) => {
+        const blog = new Blog(data.Post);
         if (blog.picture) {
           blog.picturePath = UrlService.createPictureUrl(blog.picture);
         }
         blog.longDesc = UrlService.fixPictureUrl(blog.longDesc);
-        console.log(blog, data['Post']);
+        console.log(blog, data.Post);
         this.blog = blog;
       });
     });
   }
-
 }
