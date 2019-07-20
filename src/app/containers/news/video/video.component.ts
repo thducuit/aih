8a22d19bc58f832d9043c633f86541ee503c5591
unknown <thducuit@gmail.com>
2019-07-20 08:45:55 +0700

    fix bug

--- src/app/containers/news/video/video.component.ts
+++ src/app/containers/news/video/video.component.ts
@@ -21,13 +21,18 @@ export class VideoComponent implements OnInit {
   loadVideos() {
     this.videoService.fetch().subscribe((data: any) => {
       const posts = data.Media || [];
-      this.videos = posts.map(post => {
+      const videos = posts.map(post => {
         const video = new Video(post);
         video.thumb = UrlService.createMediaUrl(video.thumb);
         return video;
       });
-      this.video = this.videos.find(item => item.hot);
-      this.videos = this.videos.filter(item => !item.hot);
+      this.video = videos.find(item => item.hot === '1');
+      this.videos = videos.filter(item => {
+        if (item.hot === '0') {
+          return item;
+        }
+      });
+      console.log('this.videos', videos);
     });
   }
 
