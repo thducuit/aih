77105c60337577f9cfd1a1bbd3ae322b987c8db6
dungjk <dungjk@gmail.com>
2019-07-27 21:30:34 +0700

    Fix customer feedbacks

--- src/app/containers/abouts/testimonial/testimonial.component.ts
+++ src/app/containers/abouts/testimonial/testimonial.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TestimonialService } from 'src/app/services/testimonial.service';
 import { Testimonial } from 'src/app/models/testimonial';
 import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-testimonial',
   templateUrl: './testimonial.component.html',
   styleUrls: ['./testimonial.component.scss'],
 })
-export class TestimonialComponent implements OnInit {
+export class TestimonialComponent implements OnInit, OnDestroy {
   public currentPage = 1;
   public testimonials: any[];
+  private subcription: Subscription;
 
   constructor(
     private testimonialService: TestimonialService,
@@ -19,11 +21,15 @@ export class TestimonialComponent implements OnInit {
 
   ngOnInit() {
     this.loadTestimonials();
-    this.translate
+    this.subcription = this.translate
       .onLangChange
       .subscribe(() => {
         this.loadTestimonials();
       });
+  }
+
+  ngOnDestroy() {
+    this.subcription.unsubscribe();
   }
 
   loadTestimonials() {
