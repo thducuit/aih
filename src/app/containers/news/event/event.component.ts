a83edab0719d077b59f1a0e30c96b134148127c5
Dung.Dang <dungjk@gmail.com>
2019-08-02 02:21:51 +0700

    Fix news page

--- src/app/containers/news/event/event.component.ts
+++ src/app/containers/news/event/event.component.ts
@@ -1,94 +1,104 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
-import {Blog} from '../../../models/blog';
-import {UrlService} from '../../../services/url.service';
-import {BlogService} from '../../../services/blog.service';
-import {CalculatePagination} from '../../../utilities';
-import {TranslateService} from '@ngx-translate/core';
-import {Subscription} from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Blog } from '../../../models/blog';
+import { UrlService } from '../../../services/url.service';
+import { BlogService } from '../../../services/blog.service';
+import { CalculatePagination } from '../../../utilities';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
-    selector: 'app-event',
-    templateUrl: './event.component.html',
-    styleUrls: ['./event.component.scss']
+  selector: 'app-event',
+  templateUrl: './event.component.html',
+  styleUrls: ['./event.component.scss'],
 })
 export class EventComponent implements OnInit, OnDestroy {
+  public blogs: Array<Blog> = [];
+  public totalRecord = 0;
+  public currentPage = 1;
+  public perPage = 7;
+  public filterBlogs = [];
+  public pages = [];
 
-    public blogs: Array<Blog> = [];
-    public totalRecord = 0;
-    public currentPage = 1;
-    public perPage = 7;
-    public filterBlogs = [];
-    public pages = [];
+  public pageNumbers: number[] = [];
+  private subscription: Subscription;
 
-    public pageNumbers: number[] = [];
-    private subscription: Subscription;
+  constructor(
+    public blogService: BlogService,
+    private translate: TranslateService,
+  ) {}
 
-    constructor(public blogService: BlogService,
-                private translate: TranslateService) {
+  ngOnInit() {
+    this.loadNews();
+    this.subscription = this.translate.onLangChange.subscribe(() => {
+      this.loadNews();
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  loadNews() {
+    this.blogService.fetch(1, 999).subscribe((data: any) => {
+      const posts = data.Posts || [];
+      this.totalRecord = posts.TotalRecord;
+      const convertedBlogs: any[] = posts.map(post => {
+        const blog = new Blog(post);
+        blog.picturePath = UrlService.createPictureUrl(blog.picture);
+        blog.url = UrlService.createNewsDetailUrl(blog.alias);
+        return blog;
+      });
+      this.blogs = convertedBlogs;
+      this.pagination();
+      this.calcPages();
+    });
+  }
+
+  pagination() {
+    const start =
+      (this.currentPage - 1) * this.perPage + (this.currentPage - 1);
+    this.filterBlogs = this.blogs.slice(start, start + this.perPage);
+  }
+
+  calcPages() {
+    const pages = Math.floor(this.blogs.length / this.perPage);
+    this.pages = [];
+    for (let i = 1; i <= pages; i++) {
+      this.pages.push(i);
     }
+  }
 
-    ngOnInit() {
-        this.loadNews();
-        this.subscription = this.translate
-            .onLangChange
-            .subscribe(() => {
-                this.loadNews();
-            });
-    }
+  selectPage(pageNum: number) {
+    this.currentPage = pageNum;
+    this.recalculatePages();
+  }
 
-    ngOnDestroy() {
-        this.subscription.unsubscribe();
-    }
+  private recalculatePages() {
+    this.pageNumbers = CalculatePagination(
+      this.currentPage,
+      this.totalRecord || 0,
+    );
+  }
 
-    loadNews() {
-        this.blogService.fetch(1, 999).subscribe((data: any) => {
-            const posts = data.Posts || [];
-            this.totalRecord = posts.TotalRecord;
-            this.blogs = posts.map(post => {
-                const blog = new Blog(post);
-                blog.picturePath = UrlService.createPictureUrl(blog.picture);
-                blog.url = UrlService.createNewsDetailUrl(blog.alias);
-                return blog;
-            });
-            this.pagination();
-            this.calcPages();
-        });
-    }
+  changePage(page) {
+    this.currentPage = page;
+    this.pagination();
+  }
 
-    pagination() {
-        const start = (this.currentPage - 1) * this.perPage + (this.currentPage - 1);
-        this.filterBlogs = this.blogs.slice(start, start + this.perPage);
-    }
+  prev() {
+    this.currentPage -= 1;
+    this.pagination();
+  }
 
-    calcPages() {
-        const pages = Math.floor(this.blogs.length / this.perPage);
-        this.pages = [];
-        for (let i = 1; i <= pages; i++) {
-            this.pages.push(i);
-        }
-    }
+  next() {
+    this.currentPage += 1;
+    this.pagination();
+  }
 
-    selectPage(pageNum: number) {
-        this.currentPage = pageNum;
-        this.recalculatePages();
+  truncateText(str: string, length: number) {
+    if (!str || str.length < length) {
+      return str;
     }
-
-    private recalculatePages() {
-        this.pageNumbers = CalculatePagination(this.currentPage, this.totalRecord || 0);
-    }
-
-    changePage(page) {
-        this.currentPage = page;
-        this.pagination();
-    }
-
-    prev() {
-        this.currentPage -= 1;
-        this.pagination();
-    }
-
-    next() {
-        this.currentPage += 1;
-        this.pagination();
-    }
+    return str.substr(0, length - 3).trim() + '...';
+  }
 }
