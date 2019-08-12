c739ccf9947a9a9b8945ab153d6df6f53b630974
Duc Nguyen <duc.nguyen@persol.co.jp>
2019-08-12 13:19:13 +0700

    https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
 
-  backend: 'http://backend.aih.tqdesign.vn',
-  api: 'http://api.aih.tqdesign.vn/',
+  backend: 'https://backend.aih.tqdesign.vn',
+  api: 'https://api.aih.tqdesign.vn/',
   mapApiKey: 'AIzaSyD77nJoVaeLUFJAGd_wp0qmTiNUJemumnE',
   secretKey: 'GwBqWPA4ezINGtLPzwNilw==',
-  host: 'http://aih.tqdesign.vn'
+  host: 'https://aih.tqdesign.vn'
 };
