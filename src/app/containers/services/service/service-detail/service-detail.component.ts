24fa3324b71d722e9605b5c0d6bafc78b82526a3
Duc Nguyen <duc.nguyen@persol.co.jp>
2019-08-26 17:54:58 +0700

    fix bug

--- src/app/containers/services/service/service-detail/service-detail.component.ts
+++ src/app/containers/services/service/service-detail/service-detail.component.ts
@@ -1,113 +1,124 @@
-import { Component, OnInit, OnDestroy, ViewChild, ChangeDetectorRef, AfterViewInit, Renderer2, ElementRef } from '@angular/core';
-import { Clinic } from '../../../../models/clinic';
-import { ActivatedRoute, Router } from '@angular/router';
-import { UrlService } from '../../../../services/url.service';
-import { CategoryService } from '../../../../services/category.service';
-import { TranslateService } from '@ngx-translate/core';
-import { Subscription, forkJoin } from 'rxjs';
-import { Meta, Title } from '@angular/platform-browser';
-import { NgAnimateScrollService } from 'ng-animate-scroll';
-import { PostService } from '../../../../services/post.service';
+import {
+    Component, OnInit, OnDestroy, ViewChild, ChangeDetectorRef, AfterViewInit, Renderer2, ElementRef, AfterViewChecked
+} from '@angular/core';
+import {Clinic} from '../../../../models/clinic';
+import {ActivatedRoute, Router} from '@angular/router';
+import {UrlService} from '../../../../services/url.service';
+import {CategoryService} from '../../../../services/category.service';
+import {TranslateService} from '@ngx-translate/core';
+import {Subscription, forkJoin} from 'rxjs';
+import {Meta, Title} from '@angular/platform-browser';
+import {NgAnimateScrollService} from 'ng-animate-scroll';
+import {PostService} from '../../../../services/post.service';
 
 @Component({
-  selector: 'app-service-detail',
-  templateUrl: './service-detail.component.html',
-  styleUrls: ['./service-detail.component.scss'],
+    selector: 'app-service-detail',
+    templateUrl: './service-detail.component.html',
+    styleUrls: ['./service-detail.component.scss'],
 })
-export class ServiceDetailComponent implements OnInit, OnDestroy, AfterViewInit {
-  public clinic: Clinic;
-  private subscription: Subscription;
-  public clinicIds;
+export class ServiceDetailComponent implements OnInit, OnDestroy, AfterViewInit, AfterViewChecked {
+    public clinic: Clinic;
+    private subscription: Subscription;
+    public clinicIds;
 
-  public minHeight = 'auto';
+    public minHeight = 'auto';
 
-  @ViewChild('serviceCate', { static: false }) serviceCate;
-  @ViewChild('copyCate', { static: false }) copyCate;
-  @ViewChild('pServiceCate', { static: false }) contentEl: ElementRef;
+    @ViewChild('serviceCate', {static: false}) serviceCate;
+    @ViewChild('copyCate', {static: false}) copyCate;
+    @ViewChild('pServiceCate', {static: false}) contentEl: ElementRef;
 
-  constructor(
-    private route: ActivatedRoute,
-    public categoryService: CategoryService,
-    public postService: PostService,
-    private translate: TranslateService,
-    private metaService: Meta,
-    private router: Router,
-    private animateScrollService: NgAnimateScrollService,
-    private titleService: Title,
-    private cdr: ChangeDetectorRef,
-    private renderer: Renderer2
-  ) {
-  }
+    constructor(private route: ActivatedRoute,
+                public categoryService: CategoryService,
+                public postService: PostService,
+                private translate: TranslateService,
+                private metaService: Meta,
+                private router: Router,
+                private animateScrollService: NgAnimateScrollService,
+                private titleService: Title,
+                private cdr: ChangeDetectorRef,
+                private renderer: Renderer2) {
+    }
 
-  ngOnInit() {
-    // const alias = this.route.snapshot.paramMap.get('alias');
-    this.route.paramMap.subscribe(params => {
-      const alias = params.get('alias');
-      this.loadCategories(alias);
-    });
-    this.subscription = this.translate.onLangChange.subscribe(() => {
-      const alias = this.route.snapshot.params['alias'];
-      this.postService.getAlias(alias).subscribe((data: any) => {
-        const newAlias = data['alias'];
-        if (newAlias) {
-          return this.router.navigate([UrlService.createClinicDetailUrl(newAlias)]);
-        } else {
-          return this.router.navigate(['/patient-services/medical-services']);
-        }
-      });
-    });
+    ngOnInit() {
+        // const alias = this.route.snapshot.paramMap.get('alias');
+        this.route.paramMap.subscribe(params => {
+            const alias = params.get('alias');
+            this.loadCategories(alias);
+        });
+        this.subscription = this.translate.onLangChange.subscribe(() => {
+            const alias = this.route.snapshot.params['alias'];
+            this.postService.getAlias(alias).subscribe((data: any) => {
+                const newAlias = data['alias'];
+                if (newAlias) {
+                    return this.router.navigate([UrlService.createClinicDetailUrl(newAlias)]);
+                } else {
+                    return this.router.navigate(['/patient-services/medical-services']);
+                }
+            });
+        });
+    }
 
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 
-  }
+    ngAfterViewInit() {
+        this.fixHeight();
+    }
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
+    private loadCategories(alias) {
+        forkJoin(
+            this.categoryService.fetch(alias, 'clinic'),
+            this.translate.get('american_international_hospital'),
+        ).subscribe(([data, aihStr]) => {
+            const clinic = new Clinic(data['Category']);
+            if (clinic.picture) {
+                clinic.picturePath = UrlService.createPictureUrl(clinic.picture);
+            }
+            clinic.longDesc = UrlService.fixPictureUrl(clinic.longDesc);
+            this.clinic = clinic;
 
-  ngAfterViewInit() {
-      const serviceCateHeight = this.serviceCate.nativeElement.offsetHeight;
-      const copyCateHeight = this.copyCate.nativeElement.offsetHeight;
-      const copyCateSection = this.contentEl.nativeElement.querySelector('.copy-cate');
-      const minHeight = serviceCateHeight > copyCateHeight ? `${serviceCateHeight}px` : 'auto';
-      this.renderer.setStyle(copyCateSection, 'min-height', minHeight);
-  }
+            this.clinicIds = [clinic.id];
 
-  private loadCategories(alias) {
-    forkJoin(
-      this.categoryService.fetch(alias, 'clinic'),
-      this.translate.get('american_international_hospital'),
-    ).subscribe(([data, aihStr]) => {
-      const clinic = new Clinic(data['Category']);
-      if (clinic.picture) {
-        clinic.picturePath = UrlService.createPictureUrl(clinic.picture);
-      }
-      clinic.longDesc = UrlService.fixPictureUrl(clinic.longDesc);
-      this.clinic = clinic;
+            // seo
+            const pageTitle = `${this.clinic.metaTitle ||
+            this.clinic.name} - ${aihStr}`;
 
-      this.clinicIds = [clinic.id];
+            this.titleService.setTitle(pageTitle);
 
-      // seo
-      const pageTitle = `${this.clinic.metaTitle ||
-      this.clinic.name} - ${aihStr}`;
+            this.metaService.updateTag({
+                property: 'og:title',
+                content: pageTitle,
+            });
+            this.metaService.updateTag({
+                name: 'description',
+                content: this.clinic.metaDesc,
+            });
+            this.metaService.updateTag({
+                name: 'keywords',
+                content: this.clinic.metaKey,
+            });
 
-      this.titleService.setTitle(pageTitle);
+            this.fixHeight();
 
-      this.metaService.updateTag({
-        property: 'og:title',
-        content: pageTitle,
-      });
-      this.metaService.updateTag({
-        name: 'description',
-        content: this.clinic.metaDesc,
-      });
-      this.metaService.updateTag({
-        name: 'keywords',
-        content: this.clinic.metaKey,
-      });
+            setTimeout(() => {
+                this.animateScrollService.scrollToElement('headerPage', 150);
+            }, 100);
+        });
+    }
 
-      setTimeout(() => {
-        this.animateScrollService.scrollToElement('headerPage', 150);
-      }, 100);
-    });
-  }
+    handleLoadCateFinish() {
+        this.fixHeight();
+    }
+
+    fixHeight() {
+        setTimeout( () => {
+            if (this.serviceCate && this.copyCate) {
+                const serviceCateHeight = this.serviceCate.nativeElement.offsetHeight;
+                const copyCateHeight = this.copyCate.nativeElement.offsetHeight;
+                this.minHeight = serviceCateHeight > copyCateHeight ? `${serviceCateHeight}px` : 'auto';
+                console.log(serviceCateHeight, copyCateHeight);
+            }
+        }, 100);
+    }
 }
--- src/app/containers/services/service/sidebar/sidebar.component.ts
+++ src/app/containers/services/service/sidebar/sidebar.component.ts
@@ -1,46 +1,47 @@
-import { Component, EventEmitter, OnInit, Output, OnDestroy } from '@angular/core';
-import { Clinic } from '../../../../models/clinic';
-import { ClinicService } from '../../../../services/clinic.service';
-import { UrlService } from '../../../../services/url.service';
-import { TranslateService } from '@ngx-translate/core';
-import { Subscription } from 'rxjs';
+import {Component, EventEmitter, OnInit, Output, OnDestroy} from '@angular/core';
+import {Clinic} from '../../../../models/clinic';
+import {ClinicService} from '../../../../services/clinic.service';
+import {UrlService} from '../../../../services/url.service';
+import {TranslateService} from '@ngx-translate/core';
+import {Subscription} from 'rxjs';
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+    selector: 'app-sidebar',
+    templateUrl: './sidebar.component.html',
+    styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit, OnDestroy {
-  public clinics: Array<Clinic> = [];
-  private subscription: Subscription;
+    public clinics: Array<Clinic> = [];
+    private subscription: Subscription;
+    @Output() loadFinish = new EventEmitter<any>();
 
-  constructor(
-    public clinicService: ClinicService,
-    private translate: TranslateService
-  ) { }
+    constructor(public clinicService: ClinicService,
+                private translate: TranslateService) {
+    }
 
-  ngOnInit() {
-    this.loadClinics();
-    this.subscription = this.translate
-      .onLangChange
-      .subscribe(() => {
+    ngOnInit() {
         this.loadClinics();
-      });
-  }
+        this.subscription = this.translate
+            .onLangChange
+            .subscribe(() => {
+                this.loadClinics();
+            });
+    }
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 
-  loadClinics() {
-    this.clinicService.fetch().subscribe((data: {}) => {
-      const posts = data['Categories'] || [];
-      this.clinics = posts.map(post => {
-        const clinic = new Clinic(post);
-        clinic.url = UrlService.createClinicDetailUrl(clinic.alias);
-        return clinic;
-      });
-    });
-  }
+    loadClinics() {
+        this.clinicService.fetch().subscribe((data: {}) => {
+            const posts = data['Categories'] || [];
+            this.clinics = posts.map(post => {
+                const clinic = new Clinic(post);
+                clinic.url = UrlService.createClinicDetailUrl(clinic.alias);
+                return clinic;
+            });
+            this.loadFinish.emit(true);
+        });
+    }
 
 }
