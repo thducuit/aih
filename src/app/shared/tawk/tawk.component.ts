96fffc0e026280e98af2a99b5336c3d069c545a0
dungjk <dungjk@gmail.com>
2019-08-29 14:36:17 +0700

    Attempt to fix Tawk UI & FB UI intergration

--- src/app/shared/tawk/tawk.component.ts
+++ src/app/shared/tawk/tawk.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Renderer2, Inject, Input } from '@angular/core';
+import {
+  Component,
+  Renderer2,
+  Inject,
+  Input,
+  AfterViewChecked,
+  NgZone,
+} from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
@@ -6,24 +13,31 @@ import { DOCUMENT } from '@angular/common';
   templateUrl: './tawk.component.html',
   styleUrls: ['./tawk.component.scss'],
 })
-export class TawkComponent implements OnInit {
+export class TawkComponent implements AfterViewChecked {
   @Input() id: string;
   constructor(
     private renderer: Renderer2,
     @Inject(DOCUMENT) private document,
+    private zone: NgZone,
   ) {}
 
-  ngOnInit() {
-    const s = this.renderer.createElement('script');
-    s.text = `var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-    (function () {
-      var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-      s1.async = true;
-      s1.src = 'https://embed.tawk.to/${this.id}/default';
-      s1.charset = 'UTF-8';
-      s1.setAttribute('crossorigin', '*');
-      s0.parentNode.insertBefore(s1, s0);
-    })();`;
-    this.renderer.appendChild(this.document.body, s);
+  ngAfterViewChecked() {
+    this.appendScript();
+  }
+
+  private appendScript() {
+    this.zone.runOutsideAngular(() => {
+      const s = this.renderer.createElement('script');
+      s.text = `var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+      (function () {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/${this.id}/default';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+      })();`;
+      this.renderer.appendChild(this.document.body, s);
+    });
   }
 }
