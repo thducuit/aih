f8ff3948d3efa45619196896ebcaf82e283d625d
dungjk <dungjk@gmail.com>
2019-08-30 16:29:49 +0700

    Fix Safe HTML

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
 
-  backend: 'https://backend.aih.tqdesign.vn',
-  api: 'https://api.aih.tqdesign.vn/',
+  backend: 'https://backend.aih.com.vn',
+  api: 'https://venus.aih.com.vn/',
   mapApiKey: 'AIzaSyD77nJoVaeLUFJAGd_wp0qmTiNUJemumnE',
   secretKey: 'GwBqWPA4ezINGtLPzwNilw==',
-  host: 'https://aih.tqdesign.vn',
+  host: 'https://aih.com.vn',
   recaptchaSiteKey: '6LeHVrIUAAAAAOwn3IAMUMA1TFetq2Fuw8yB5WjK'
 };
