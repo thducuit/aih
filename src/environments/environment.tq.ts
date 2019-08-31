7fbb422788fa9e2b0bd09d50e35c7c26858135c4
dungjk <dungjk@gmail.com>
2019-08-31 14:23:51 +0700

    Add build config & fix build config ref

--- src/environments/environment.tq.ts
+++ src/environments/environment.tq.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
 
-  backend: 'https://backend.aih.com.vn',
-  api: 'https://venus.aih.com.vn/',
+  backend: 'https://backend.aih.tqdesign.vn',
+  api: 'https://api.aih.tqdesign.vn/',
   mapApiKey: 'AIzaSyD77nJoVaeLUFJAGd_wp0qmTiNUJemumnE',
   secretKey: 'GwBqWPA4ezINGtLPzwNilw==',
-  host: 'https://aih.com.vn',
+  host: 'https://aih.tqdesign.vn',
   recaptchaSiteKey: '6LeHVrIUAAAAAOwn3IAMUMA1TFetq2Fuw8yB5WjK'
 };
