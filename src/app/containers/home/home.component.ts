6275016208b9695c3fe7787e769351bb2a6f99ca
Dung.Dang <dungjk@gmail.com>
2019-09-10 07:51:58 +0700

    Prevent home page load duplicately

--- src/app/containers/home/home.component.ts
+++ src/app/containers/home/home.component.ts
@@ -1,108 +1,132 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
-import {PageService} from '../../services/page.service';
-import {Page} from '../../models/page';
-import {UrlService} from '../../services/url.service';
-import {BannerService} from '../../services/banner.service';
-import {TranslateService} from '@ngx-translate/core';
-import {Subscription, forkJoin} from 'rxjs';
-import {Meta, Title} from '@angular/platform-browser';
-import {LoaderService} from 'src/app/services/loader-service';
-import {environment} from '../../../environments/environment';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PageService } from '../../services/page.service';
+import { Page } from '../../models/page';
+import { UrlService } from '../../services/url.service';
+import { BannerService } from '../../services/banner.service';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription, forkJoin, Subject } from 'rxjs';
+import { Meta, Title } from '@angular/platform-browser';
+import { LoaderService } from 'src/app/services/loader-service';
+import { environment } from '../../../environments/environment';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
-    public page: Page;
-    public banners: any[] = [];
-    private subscription: Subscription;
-    public pageClasses: string[];
+  public page: Page;
+  public banners: any[] = [];
+  private subscription: Subscription;
+  public pageClasses: string[];
 
-    constructor(public pageService: PageService,
-                public bannerService: BannerService,
-                private translate: TranslateService,
-                private loaderService: LoaderService,
-                private metaService: Meta,
-                private titleService: Title,) {
-        this.pageClasses = this.getPageClasses();
-    }
+  private loadPageSubject = new Subject();
+  private loadPageDebounceSub = this.loadPageSubject
+    .pipe(debounceTime(150))
+    .subscribe(() => {
+      this.loadPage();
+    });
 
-    ngOnInit() {
-        this.loadPage();
-        this.subscription = this.translate.onLangChange.subscribe(() => {
-            this.loadPage();
-            this.pageClasses = this.getPageClasses();
+  constructor(
+    public pageService: PageService,
+    public bannerService: BannerService,
+    private translate: TranslateService,
+    private loaderService: LoaderService,
+    private metaService: Meta,
+    private titleService: Title,
+  ) {
+    this.pageClasses = this.getPageClasses();
+  }
+
+  ngOnInit() {
+    this.loadPageSubject.next();
+    this.loadPage();
+    this.subscription = this.translate.onLangChange.subscribe(() => {
+      this.loadPageSubject.next();
+      this.pageClasses = this.getPageClasses();
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription && this.subscription.unsubscribe();
+    this.loadPageDebounceSub && this.loadPageDebounceSub.unsubscribe();
+  }
+
+  getPageClasses() {
+    const originalLang = this.translate.currentLang;
+    const languageClass = originalLang === 'vi' ? 'vn' : originalLang;
+    return [languageClass, 'window'];
+  }
+
+  loadPage() {
+    this.loaderService.show();
+    forkJoin(
+      this.pageService.fetch('homepage'),
+      this.translate.get('american_international_hospital'),
+    ).subscribe(
+      ([homeResp, aihStr]) => {
+        const page = homeResp.Post || {};
+        this.page = new Page(page);
+        const pageTitle = `${this.page.metaTitle ||
+          this.page.name} - ${aihStr}`;
+        // seo
+        this.titleService.setTitle(pageTitle);
+        this.metaService.updateTag({
+          property: 'og:title',
+          content: pageTitle,
         });
-    }
+        this.metaService.updateTag({
+          name: 'description',
+          content: this.page.metaDesc,
+        });
+        this.metaService.updateTag({
+          property: 'og:description',
+          content: this.page.metaDesc,
+        });
+        this.metaService.updateTag({
+          name: 'keywords',
+          content: this.page.metaKey,
+        });
 
-    ngOnDestroy() {
-      if (this.subscription) {
-        this.subscription.unsubscribe();
-      }
-    }
+        this.metaService.updateTag({
+          name: 'og:url',
+          property: `${environment.host}/`,
+        });
 
-    getPageClasses() {
-        const originalLang = this.translate.currentLang;
-        const languageClass = originalLang === 'vi' ? 'vn' : originalLang;
-        return [languageClass, 'window'];
-    }
+        if (this.page.picture) {
+          this.metaService.updateTag({
+            property: 'og:image',
+            content: UrlService.createPictureUrl(this.page.picture),
+          });
+        }
 
-    loadPage() {
-        this.loaderService.show();
-        forkJoin(
-            this.pageService.fetch('homepage'),
-            this.translate.get('american_international_hospital'),
-        ).subscribe(
-            ([homeResp, aihStr]) => {
-                const page = homeResp.Post || {};
-                this.page = new Page(page);
-                const pageTitle = `${this.page.metaTitle || this.page.name} - ${aihStr}`;
-                // seo
-                this.titleService.setTitle(pageTitle);
-                this.metaService.updateTag({
-                    property: 'og:title',
-                    content: pageTitle,
-                });
-                this.metaService.updateTag({
-                    name: 'description',
-                    content: this.page.metaDesc,
-                });
-                this.metaService.updateTag({
-                    property: 'og:description',
-                    content: this.page.metaDesc,
-                });
-                this.metaService.updateTag({
-                    name: 'keywords',
-                    content: this.page.metaKey,
-                });
-
-                this.metaService.updateTag({name: 'og:url', property: `${environment.host}/`});
-
-                if (this.page.picture) {
-                    this.metaService.updateTag({
-                        property: 'og:image',
-                        content: UrlService.createPictureUrl(this.page.picture),
-                    });
-                }
-
-                this.pageService
-                    .fetchBanner('home_slide')
-                    .subscribe((bannersResp: any) => {
-                        const banners = bannersResp['Posts'] || [];
-                        this.banners = banners.map(item => {
-                            const meta = item.post_meta ? JSON.parse(item.post_meta) : {};
-                            meta.large = UrlService.createPictureUrl(meta.picture, null, null, true);
-                            meta.small = UrlService.createPictureUrl(meta.picture_mobile, null, null, true);
-                            return meta;
-                        });
-                    });
-            },
-            null,
-            () => {
-                this.loaderService.hide();
-            },
-        );
-    }
+        this.pageService
+          .fetchBanner('home_slide')
+          .subscribe((bannersResp: any) => {
+            const banners = bannersResp['Posts'] || [];
+            this.banners = banners.map(item => {
+              const meta = item.post_meta ? JSON.parse(item.post_meta) : {};
+              meta.large = UrlService.createPictureUrl(
+                meta.picture,
+                null,
+                null,
+                true,
+              );
+              meta.small = UrlService.createPictureUrl(
+                meta.picture_mobile,
+                null,
+                null,
+                true,
+              );
+              return meta;
+            });
+          });
+      },
+      null,
+      () => {
+        this.loaderService.hide();
+      },
+    );
+  }
 }
