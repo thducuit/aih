39fe4d00ace1cff64471be1b7b7eba26f2b22e41
Duc Nguyen <duc.nguyen@persol.co.jp>
2020-04-28 15:05:30 +0700

    fix layout insuarance

--- src/app/services/url.service.ts
+++ src/app/services/url.service.ts
@@ -1,81 +1,81 @@
-import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
+import {Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
 
 @Injectable()
 export class UrlService {
 
-  constructor() {
-  }
-
-  static createPictureUrl(pictureName, size = null, type = 'post', isMeta = false) {
-    if (isMeta) {
-      return `${environment.backend}${pictureName}`;
+    constructor() {
     }
-    if (size) {
-      return `${environment.backend}/assets/uploads/images/${type}/thumbs/${size}/${pictureName}`;
+
+    static createPictureUrl(pictureName, size = null, type = 'post', isMeta = false) {
+        if (isMeta) {
+            return `${environment.backend}${pictureName}`;
+        }
+        if (size) {
+            return `${environment.backend}/assets/uploads/images/${type}/thumbs/${size}/${pictureName}`;
+        }
+        return `${environment.backend}/assets/uploads/images/${type}/${pictureName}`;
     }
-    return `${environment.backend}/assets/uploads/images/${type}/${pictureName}`;
-  }
 
-  static createNewsDetailUrl(alias = null) {
-    if (alias) {
-      return `/news/detail/${alias}`;
+    static createNewsDetailUrl(alias = null) {
+        if (alias) {
+            return `/news/detail/${alias}`;
+        }
+        return '/news';
     }
-    return '/news';
-  }
 
-  static createMemberDetailUrl(alias = null) {
-    if (alias) {
-      return `/insurance/membership/${alias}`;
+    static createMemberDetailUrl(alias = null) {
+        if (alias) {
+            return `/insurance/membership/${alias}`;
+        }
+        return '/insurance/membership';
     }
-    return '/insurance/membership';
-  }
 
-  static createInsuranceUrl(alias = null) {
-    if (alias) {
-      return `/insurance/insurance-detail/${alias}`;
+    static createInsuranceUrl(alias = null) {
+        if (alias) {
+            return `/insurance/insurance-detail/${alias}`;
+        }
+        return '/insurance/insurance-detail';
     }
-    return '/insurance/insurance-detail';
-  }
 
-  static createCareerDetailUrl(alias = null) {
-    if (alias) {
-      return `/career/detail/${alias}`;
+    static createCareerDetailUrl(alias = null) {
+        if (alias) {
+            return `/career/detail/${alias}`;
+        }
+        return '/career';
     }
-    return '/career';
-  }
 
-  static createDoctorDetailUrl(alias = null) {
-    if (alias) {
-      return `/doctor/detail/${alias}`;
+    static createDoctorDetailUrl(alias = null) {
+        if (alias) {
+            return `/doctor/detail/${alias}`;
+        }
+        return '/doctor';
     }
-    return '/doctor';
-  }
 
-  static createClinicDetailUrl(alias = null) {
-    if (alias) {
-      return `/patient-services/medical-services/${alias}`;
+    static createClinicDetailUrl(alias = null) {
+        if (alias) {
+            return `/patient-services/medical-services/${alias}`;
+        }
+        return '/patient-services/medical-services';
     }
-    return '/patient-services/medical-services';
-  }
 
-  static createInsuranceDetailUrl(id, alias) {
-    const baseUrl = alias === 'insurance' ? '/insurance/insurance-consulting/' : '/insurance/membership-consulting/';
-    return `${baseUrl}${id}`;
-  }
+    static createInsuranceDetailUrl(id, alias) {
+        const baseUrl = (alias === 'insurance' || alias === 'bao-hiem') ? '/insurance/insurance-consulting/' : '/insurance/membership-consulting/';
+        return `${baseUrl}${id}`;
+    }
 
-  static createMediaUrl(thumb) {
-    return `${environment.backend}${thumb}`;
-  }
+    static createMediaUrl(thumb) {
+        return `${environment.backend}${thumb}`;
+    }
 
-  static fixPictureUrl(text) {
-	if(text.indexOf('http') === 0) {
-		return text;
-	}
-    return `${text}`.replace(/\/assets/g, `${environment.backend}/assets`);
-  }
+    static fixPictureUrl(text) {
+        if (text && text.indexOf('http') === 0) {
+            return text;
+        }
+        return `${text}`.replace(/\/assets/g, `${environment.backend}/assets`);
+    }
 
-  static createIframeUrl(code) {
-    return `https://www.youtube.com/embed/${code}?rel=0&modestbranding=1&autoplay=1&showinfo=0&controls=1`;
-  }
+    static createIframeUrl(code) {
+        return `https://www.youtube.com/embed/${code}?rel=0&modestbranding=1&autoplay=1&showinfo=0&controls=1`;
+    }
 }
